11799570607291f6b0dfffe66da3348ec1b95f54
liweijian <liweijian@yingu.com>
2017-07-04 19:29:42 +0800

    Initial commit

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jack on 2017/7/4.
+ */
+@RestController
+public class UserController {
+}
---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jack on 2017/7/4.
+ */
+@Service
+public class UserService {
+}
